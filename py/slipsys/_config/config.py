6ac023fc567526f9145e816c35331eac1f0608b5
ardnico <leaf.sun2@gmail.com>
2022-11-06 00:19:55 +0900

    runserver

--- py/slipsys/_config/config.py
+++ py/slipsys/_config/config.py
@@ -6,6 +6,7 @@ class config:
         default_data_dir = fr"{os.getcwd()}\data"
         path_setting = fr"{default_data_dir}\path_setting.data"
         os.makedirs(default_data_dir,exist_ok=True)
+        self.startup_file = os.environ["APPDATA"] + r"\Microsoft\Windows\Start Menu\Programs\Startup\update_slipsys.bat"
         if os.path.exists(path_setting):
             with open(path_setting,"r",encoding="shift_jis") as f:
                 self.setting_path = f.read()
@@ -24,3 +25,30 @@ python SlipSys.py
 """
             with open(fr"{user_desktop}\start_app.bat","w",encoding="shift_jis") as f:
                 f.write(starter_txt)
+        rewrite_file = fr"{default_data_dir}\rewrite_file.csv"
+        if os.path.exists(rewrite_file):
+            with open(rewrite_file,"r",encoding="shift_jis") as f:
+                rewrite_txt = f.read().split("\n")
+            for line in rewrite_txt:
+                tmp_line = line.split(",")
+                if len(tmp_line) != 2:
+                    continue
+                self.rewrite_startup_file(tmp_line[0],int(tmp_line[1]))
+        else:
+            with open(rewrite_file,"w",encoding="shift_jis") as f:
+                f.write("")
+    
+    def rewrite_startup_file(self,text,mode=0):
+        with open(self.startup_file,"r",encoding="shift_jis") as f:
+            tmp_txt = f.read()
+        if mode == 0:
+            # add text
+            with open(self.startup_file,"a",encoding="shift_jis") as f:
+                f.write(text)
+        elif mode == 1:
+            # remove text
+            tmp_txt = tmp_txt.split("\n")
+            tmp_txt = [i for i in tmp_txt if i != text]
+            tmp_txt = "\n".join(tmp_txt)
+            with open(self.startup_file,"w",encoding="shift_jis") as f:
+                f.write(tmp_txt)
--- py/slipsys/slipsys.py
+++ py/slipsys/slipsys.py
@@ -1,14 +1,31 @@
+
+import os
 import _config as cfg
+import subprocess
+from multiprocessing import Process
 
 class slipsys:
     config = cfg.config()
     def __init__(self) -> None:
         pass
     
+    def run_server(self):
+        os.chdir(r"C:\work_space\SlipSys\py\slipsys\django")
+        subprocess.run(["python","manage.py","runserver","127.0.0.1:8000"])
+    
+    def run_by_process(self):
+        serverprocess = Process(target=self.run_server ,args=())
+        serverprocess.start()
+        return serverprocess
+    
+    def launch_webbrowser(self):
+        pass
     
     def main(self):
-        print('comming soon ...')
+        self.run_by_process()
+        print('comming soon ...')        
 
 if __name__ == "__main__":
     instance = slipsys()
     instance.main()
+    # instance.run_server()
